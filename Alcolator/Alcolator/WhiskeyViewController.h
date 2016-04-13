320056079e99bcdeba40dc9176dcf4cf6fc95619
Eddy Chan <echan162@gmail.com>
2016-04-13 16:54:20 -0700

    Checkpoint 20 alcolator storyboards code: completed

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Eddy Chan on 4/13/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
