3707ac39c9306aedf1e740f1cb6b02949b7615d9
Eddy Chan <echan162@gmail.com>
2016-04-12 20:48:40 -0700

    Checkpoint 19 alcolator storyboards: completed

---
+++ Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Eddy Chan on 4/12/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
